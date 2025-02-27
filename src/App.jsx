0ee4d7af1bf0cfd2c896284699c47367b6e2455b
Yunusr714 <yunusabdul274@gmail.com>
2025-02-27 23:20:23 +0530

    modified app

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,13 @@ import Presenties from './Pages/presenties_page/Presenties'
 import Login from './Pages/Login/Login';
 import FacultyDashboard from './Pages/faculty_dashboard/FacultyDashboard';
 import FacultyRegister from './Pages/facultyRegister/FacultyRegister';
-<<<<<<< HEAD
-import StudentAttendance from './Pages/StudentAttendance/StudentAttendance';
-=======
 import Studentdashboard from './Pages/StudentProfile/Studentdashboard';
 import Student_Reg from './Pages/Registerations/Student_Reg';
->>>>>>> 33946cf39c7c760d0dae472b0d331b1536734379
 
 function App() {
 
   return (
     <>
-<<<<<<< HEAD
-      <div className="app">
-        <Routes>
-          <Route path="/presenties" element={<Presenties />} />
-          <Route path="/" element={<Login />} />
-          <Route path="/faculty-dashboard" element={<FacultyDashboard />} />
-          <Route path="/facultyRegister" element={<FacultyRegister />} />
-          <Route path="/attendance/:subjectId/:sectionId" element={<StudentAttendance />} />
-        </Routes>
-      </div>
-=======
         <div className="app">
           <Routes>
             <Route path="/presenties" element={<Presenties />} />
@@ -38,7 +23,6 @@ function App() {
             <Route path="/StudentRegister" element={<Student_Reg/>} />
           </Routes>
         </div>
->>>>>>> 33946cf39c7c760d0dae472b0d331b1536734379
     </>
   )
 }
