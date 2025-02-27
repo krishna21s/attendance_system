33946cf39c7c760d0dae472b0d331b1536734379
Yunusr714 <yunusabdul274@gmail.com>
2025-02-27 23:14:36 +0530

    modified registertaion of stundet

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Presenties from './Pages/presenties_page/Presenties'
 import Login from './Pages/Login/Login';
 import FacultyDashboard from './Pages/faculty_dashboard/FacultyDashboard';
 import FacultyRegister from './Pages/facultyRegister/FacultyRegister';
+import Studentdashboard from './Pages/StudentProfile/Studentdashboard';
+import Student_Reg from './Pages/Registerations/Student_Reg';
 
 function App() {
 
@@ -17,6 +19,8 @@ function App() {
             <Route path="/login" element={<Login/>} />
             <Route path="/facultyDashboard" element={<FacultyDashboard/>} />
             <Route path="/facultyRegister" element={<FacultyRegister/>} />
+            <Route path="/StudentDashboard" element={<Studentdashboard/>} />
+            <Route path="/StudentRegister" element={<Student_Reg/>} />
           </Routes>
         </div>
     </>
